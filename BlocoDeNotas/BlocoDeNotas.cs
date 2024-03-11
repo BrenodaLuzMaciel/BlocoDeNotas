fea7342e51fe4d46c102a4e9b94c86fe5c2485e0
Breno da Luz Maciel <maciel_lpnm@hotmail.com>
2024-03-11 18:14:32 -0300

    Criando o Editor de texto

---
+++ BlocoDeNotas/BlocoDeNotas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoDeNotas
+{
+    internal class BlocoDeNotas
+    {
+        private bool FimDoPrograma;
+
+        public BlocoDeNotas()
+        {
+            FimDoPrograma = false;
+        }
+
+        public void Iniciar()
+        {
+            while (!FimDoPrograma)
+            {
+                RenderizarMenu();
+                new LerEscolhaUsuario().EscolhaMenu();
+
+            }
+        }
+
+        private void RenderizarMenu()
+        {
+            Console.Clear();
+            Console.WriteLine(EscolherMenu());
+        }
+
+        public string EscolherMenu()
+        {
+            return "1 - Iniciar editor de texto\n" +
+                   "2 - Procurar um arquivo salvo\n";
+        }
+    }
+}
---
+++ BlocoDeNotas/LerEscolhaUsuario.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoDeNotas
+{
+    internal class LerEscolhaUsuario
+    {
+        string nomeArquivo = null;
+
+        public void EscolhaMenu()
+        {
+
+            Console.Write("Escolha um numero de 1 a 2 do Indice: ");
+            bool eUmNumero = int.TryParse(Console.ReadLine(), out int escolhaIndice);
+
+            while (!eUmNumero)
+            {
+                Console.Write("Escolha um numero de 1 a 2 do Indice: ");
+                eUmNumero = int.TryParse(Console.ReadLine(), out escolhaIndice);
+            }
+
+
+            switch (escolhaIndice)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.Write("Digite o nome do seu arquivo:");
+                    nomeArquivo = Console.ReadLine();
+                    Console.WriteLine("Digite seu texto aqui:");
+                    string escritaUsuario = Console.ReadLine();
+                    ReceberEscritaUsuario(escritaUsuario);
+                    break;
+            }
+        }
+
+
+        private void ReceberEscritaUsuario(string escritaUsuario)
+        {
+            string saidaTexto = @"C:\Users\Breno\source\repos\BlocoDeNotas\" + nomeArquivo;
+
+            try
+            {
+                using (StreamWriter sw = File.CreateText(saidaTexto))
+                {
+                    sw.WriteLine(escritaUsuario);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu!" + e);
+            }
+
+        }
+    }
+}
